37dbff21b136613410c1ced333499cebc7e99e98
dineshNehete <nehetedinesh7@gmail.com>
2022-12-04 02:16:14 +0530

    Setting the UI

--- Quiz/app/src/main/java/com/example/quiz/QuizQuestionsActivity.kt
+++ Quiz/app/src/main/java/com/example/quiz/QuizQuestionsActivity.kt
@@ -3,12 +3,47 @@ package com.example.quiz
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.ImageView
+import android.widget.ProgressBar
+import android.widget.TextView
 
 class QuizQuestionsActivity : AppCompatActivity() {
+    private var progressBar: ProgressBar? = null
+    private var tvProgress: TextView? = null
+    private var tvQuestion: TextView? = null
+    private var ivImage: ImageView? = null
+
+    private var tvOptionOne: TextView? = null
+    private var tvOptionTwo: TextView? = null
+    private var tvOptionThree: TextView? = null
+    private var tvOptionFour: TextView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz_questions2)
         val questionsList = Constants.getQuestions()
-        Log.i("Question list size is: " ,"${questionsList.size}")
+        Log.i("Question list size is: ", "${questionsList.size}")
+
+        progressBar = findViewById(R.id.progressBar)
+        tvProgress = findViewById(R.id.tv_progress)
+        tvQuestion = findViewById(R.id.tv_question)
+        ivImage = findViewById(R.id.iv_image)
+        tvOptionOne = findViewById(R.id.tv_option_one)
+        tvOptionTwo = findViewById(R.id.tv_option_two)
+        tvOptionThree = findViewById(R.id.tv_option_three)
+        tvOptionFour = findViewById(R.id.tv_option_four)
+
+        val currentPosition = 1
+        val question: Question? =
+            questionsList[currentPosition - 1]
+        progressBar?.progress = currentPosition // Setting the current progress in the progressbar using the position of question
+        tvProgress?.text = "$currentPosition" + "/" + progressBar?.getMax() // Setting up the progress text
+
+        // Now set the current question and the options in the UI
+        tvQuestion?.text = question!!.question
+        ivImage?.setImageResource(question.image)
+        tvOptionOne?.text = question.optionOne
+        tvOptionTwo?.text = question.optionTwo
+        tvOptionThree?.text = question.optionThree
+        tvOptionFour?.text = question.optionFour
     }
 }
