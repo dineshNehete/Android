e158039ea7800985d252753ad7f3bfb40351f2dc
dineshNehete <nehetedinesh7@gmail.com>
2022-12-11 02:47:53 +0530

    Multiple Permissions added

--- Permissions/app/src/main/java/com/example/permissions/MainActivity.kt
+++ Permissions/app/src/main/java/com/example/permissions/MainActivity.kt
@@ -24,6 +24,47 @@ class MainActivity : AppCompatActivity() {
                 Toast.makeText(this, "Permission Not Granted", Toast.LENGTH_SHORT).show()
             }
         }
+    private val cameraAndLocationLauncher: ActivityResultLauncher<Array<String>> =
+        registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()
+        ) { permissions ->
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                permissions.entries.forEach {
+                    // it represents the Map of string and boolean, builtin
+                    val permissionName = it.key
+                    val isGranted = it.value
+                    if (isGranted) {
+                        if (permissionName == Manifest.permission.ACCESS_FINE_LOCATION) {
+                            Toast.makeText(
+                                this,
+                                "Permission Granted for location",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        } else {
+                            Toast.makeText(
+                                this,
+                                "Permission Granted for camera",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    } else {
+                        if (permissionName == Manifest.permission.ACCESS_FINE_LOCATION) {
+                            Toast.makeText(
+                                this,
+                                "Permission denied for location",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        } else {
+                            Toast.makeText(
+                                this,
+                                "Permission denied for camera",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    }
+                }
+            }
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,8 +83,8 @@ class MainActivity : AppCompatActivity() {
             } else {
                 // Directly ask for the permission.
                 // The registered ActivityResultCallback gets the result of this request.
-                cameraResultLauncher.launch(
-                    Manifest.permission.CAMERA
+                cameraAndLocationLauncher.launch(
+                    arrayOf(Manifest.permission.CAMERA, Manifest.permission.ACCESS_FINE_LOCATION)
                 )
             }
 
